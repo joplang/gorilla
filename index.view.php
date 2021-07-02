6cc27fdc7691e466cbfcb3c73dbbc650c75853ad
joplang <joplang@hotmail.com>
2021-07-02 09:25:34 +0200

    eerste oefening

--- index.view.php
+++ index.view.php
@@ -1,20 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gorilla</title>
+    <title>PhP-oefening</title>
 </head>
-
 <body>
-    <header>
-        <h1>Dit is een pagina over gorilla's</h1>
-    </header>
-    <section>
-        <p>dat zijn apen</p>
-    </section>
+    
 </body>
-
 </html>

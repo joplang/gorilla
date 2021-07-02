5b0b1bbc243154898cbbf6b5122aa2c308f799d4
joplang <joplang@hotmail.com>
2021-07-02 14:16:22 +0200

    oefen

--- index.php
+++ index.php
@@ -1,2 +1,35 @@
-<?php
+<?php 
 
+require 'functions.php';
+
+
+$names = [
+    'Joop',
+    'Jaap',
+    'Jeroen',
+    'Jochem'
+];
+
+$animals = [
+    'Hert',
+    'Aap',
+    'Muis',
+    'Uil',
+    'papagaaiduiker',
+];
+
+$persoon = [
+    'naam' => 'Joppe',
+    'leeftijd' => 27,
+    'baard' => true,
+];
+
+$task = [
+    'title' => 'huiswerk',
+    'date' => 'vandaag',
+    'name' => 'Joppe',
+    'done' => true,
+];
+
+
+require 'index.view.php';
